13f16278eb6a606cdb97bef48ca3a24083a50d69
simon@macbook <simon@alslab.com>
2018-06-24 16:05:21 +0800

    Add headers.h file

---
+++ headers.h
@@ -0,0 +1,18 @@
+#pragma once
+
+#include <vector>
+#include <deque>
+#include <list>
+#include <array>
+#include <set> // set & multiset
+#include <map> // map & multimap
+#include <unordered_set> // unordered set/multiset
+#include <unordered_map> // unordered map/multimap
+#include <iterator>
+#include <numeric>
+#include <algorithm>
+#include <iostream>
+#include <fstream>
+#include <complex>
+#include <functional>
+#include <string>
